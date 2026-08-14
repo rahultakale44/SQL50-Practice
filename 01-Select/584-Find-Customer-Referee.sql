3445caead79f743abccc2583ba3b62a0d5654a04
Rahul Takale <174298401+rahultakale44@users.noreply.github.com>
2026-08-14 12:18:58 +0530

    Solve 584 Find Customer Referee

---
+++ 01-Select/584-Find-Customer-Referee.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS sql50_practice;
+USE sql50_practice;
+
+DROP TABLE IF EXISTS Customer;
+
+CREATE TABLE Customer (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    referee_id INT
+);
+
+INSERT INTO Customer (id, name, referee_id)
+VALUES
+(1, 'Will', NULL),
+(2, 'Jane', NULL),
+(3, 'Alex', 2),
+(4, 'Bill', NULL),
+(5, 'Zack', 1),
+(6, 'Mark', 2);
+
+SELECT name
+FROM Customer
+WHERE referee_id <> 2
+   OR referee_id IS NULL;
