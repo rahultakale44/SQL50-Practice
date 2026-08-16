3b5ce70b6328e1b70e54dc95b2fb54260da685ed
Rahul Takale <174298401+rahultakale44@users.noreply.github.com>
2026-08-17 02:09:20 +0530

    Solve 595 Big Countries

---
+++ 01-Select/595-Big-Countries.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS sql50_practice;
+USE sql50_practice;
+
+DROP TABLE IF EXISTS World;
+
+CREATE TABLE World (
+    name VARCHAR(100) PRIMARY KEY,
+    continent VARCHAR(100),
+    area INT,
+    population INT,
+    gdp BIGINT
+);
+
+INSERT INTO World (name, continent, area, population, gdp)
+VALUES
+('Afghanistan', 'Asia', 652230, 25500100, 20343000000),
+('Albania', 'Europe', 28748, 2831741, 12960000000),
+('Algeria', 'Africa', 2381741, 37100000, 188681000000),
+('Andorra', 'Europe', 468, 78115, 3712000000),
+('Angola', 'Africa', 1246700, 20609294, 100990000000);
+
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000
+   OR population >= 25000000;
