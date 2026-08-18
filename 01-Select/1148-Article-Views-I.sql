eb87fdfb5fd1b79d6339700ddc559c9e78262b29
Rahul Takale <174298401+rahultakale44@users.noreply.github.com>
2026-08-18 19:34:07 +0530

    Solve 1148 Article Views I

---
+++ 01-Select/1148-Article-Views-I.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS sql50_practice;
+USE sql50_practice;
+
+DROP TABLE IF EXISTS Views;
+
+CREATE TABLE Views (
+    article_id INT,
+    author_id INT,
+    viewer_id INT,
+    view_date DATE
+);
+
+INSERT INTO Views (article_id, author_id, viewer_id, view_date)
+VALUES
+(1, 3, 5, '2019-08-01'),
+(1, 3, 6, '2019-08-02'),
+(2, 7, 7, '2019-08-01'),
+(2, 7, 6, '2019-08-02'),
+(4, 7, 1, '2019-07-22'),
+(3, 4, 4, '2019-07-21'),
+(3, 4, 4, '2019-07-21');
+
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY id ASC;
